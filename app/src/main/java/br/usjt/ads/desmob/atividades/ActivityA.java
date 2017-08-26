e202fa346189f983ba36322a4072a9ed62392c40
viniLopesOliveira <vinicius.lopes.oliveira@outlook.com>
2017-08-26 12:06:00 -0300

    Versão final da segunda atividade

--- app/src/main/java/br/usjt/ads/desmob/atividades/ActivityA.java
+++ app/src/main/java/br/usjt/ads/desmob/atividades/ActivityA.java
@@ -1,7 +1,13 @@
 package br.usjt.ads.desmob.atividades;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class ActivityA extends Activity {
 
@@ -10,4 +16,22 @@ public class ActivityA extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_a);
     }
-}
+
+    public void startActivityB(View view){
+
+        Intent intent = new Intent(this, ActivityB.class);
+        startActivity(intent);
+    }
+    public void startActivityC(View view){
+
+        Intent intent = new Intent(this, ActivityC.class);
+        startActivity(intent);
+    }
+    public void finishActivityA(View view)
+    {
+
+        finish();
+    }
+
+
+    }
